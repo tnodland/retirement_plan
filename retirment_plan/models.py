6d965494f2a6d04a7c2bd448bd3d8ec2a4dcaca1
Trevor Nodland <trevor.nodland@gmail.com>
2020-03-31 16:18:21 -0600

    adds inital database model for the fish table

--- retirment_plan/models.py
+++ retirment_plan/models.py
@@ -6,3 +6,12 @@ class Bug(models.Model):
     location = models.CharField(null=False, max_length=255)
     time = models.TimeField()
     hemisphere = models.CharField(null=False, max_length=255)
+
+class Fish(models.Model):
+    name = models.CharField(null=False, max_length=255)
+    price = models.IntegerField(default=0)
+    water_type = models.CharField(null=False, max_length=255)
+    location = models.CharField(null=False, max_length=255)
+    time = models.TimeField()
+    hemisphere = models.CharField(null=False, max_length=255)
+    month = models.IntegerField(default=1)
